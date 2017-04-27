8a6a28414e6060986e9efaff5850ef065722965c
孟令通 <ak-7755@qq.com>
2017-04-27 21:35:56 +0800

    封装AFN 以及 SdWebImage

---
+++ LTLive/Classes/Network/LTAPIConfig.h
@@ -0,0 +1,35 @@
+//
+//  LTAPIConfig.h
+//  LTLive
+//
+//  Created by 孟令通 on 2017/4/27.
+//  Copyright © 2017年 LryMlt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LTAPIConfig : NSObject
+
+#define SERVER_HOST @"http://218.11.0.112"
+
+#define IMAGE_HOST @"http://img.meelive.cn/"
+
+//首页数据
+#define API_LiveGetTop @"api/live/gettop"
+
+//广告地址
+#define API_Advertise @"advertise/get"
+
+//热门话题
+#define API_TopicIndex @"api/live/topicindex"
+
+//附近的人
+#define API_NearLocation @"api/live/near_recommend"//?uid=85149891&latitude=40.090562&longitude=116.413353
+
+// api/live/near_flow_old?uid=319901879
+
+//欢哥直播地址
+#define Live_Dahuan @"rtmp://live.hkstv.hk.lxdns.com:1935/live/dahuan"
+
+
+@end
