7fb4a82bc2bb028ca26eec0e9f6103ae33c0bdb1
孟令通 <ak-7755@qq.com>
2017-05-03 16:19:21 +0800

    添加我的头像，增加登录页面

---
+++ LTLive/Classes/Func/Mine/Controller/LTLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LTLoginViewController.h
+//  LTLive
+//
+//  Created by 孟令通 on 2017/5/3.
+//  Copyright © 2017年 LryMlt. All rights reserved.
+//
+
+#import "LTBaseViewController.h"
+
+@interface LTLoginViewController : LTBaseViewController
+
+@end
